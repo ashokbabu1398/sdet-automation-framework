00a4bc4e9d6a53b73f1d5f024eb8d8b57b84d2de
ashokbabu1398 <ashokbabu1398@gmail.com>
2026-03-26 22:10:54 +0530

    Create BaseTest.java

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,23 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://example.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
